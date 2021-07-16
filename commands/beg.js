27377c3600d5a00a2fa947ca1837b3e51056e913
AmashiSenpai <81893450+AmashiSenpai@users.noreply.github.com>
2021-07-16 09:48:23 -0700

    Create beg.js

---
+++ commands/beg.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: "beg",
+  async execute(message, args, client, Discord, ProfileData, user, userQuery){
+
+    let min = 10;
+    let max = 25;
+    // max minus the min to find the median amount
+    let median = max - min;
+    // get the amount we'll be giving to the user
+    let amount = Math.floor(Math.random() * median) + min;
+
+    // this is the same as calling "find one and update"
+    await ProfileData.updateOne(userQuery, {
+      "$inc": { "coins": amount }
+    });
+
+    await message.channel.send(`You Have Begged and Gained ${amount}`);
+  }
+}
